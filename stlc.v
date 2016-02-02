f708de03a36b57d50a610bc7638ad31d4e844831
Amin Timany <amintimany@gmail.com>
2016-02-02 13:29:50 +0100

    Completed the proof of subject reduction.
    There are some admits which are easy to prove but need some extra lemmas for context. A decision to make is whether to keep context as specially defined lists or to use lists from Coq's library.
    
    Added the _CoqProject and configure.sh script.

--- stlc.v
+++ stlc.v
@@ -1,4 +1,7 @@
-Require Import Coq.Arith.Peano_dec.
+Require Import Coq.Arith.Compare_dec.
+Require Import Coq.omega.Omega.
+
+Opaque lt_eq_lt_dec.
 
 (** Terms *)
 Inductive Trm : Type :=
@@ -32,38 +35,210 @@ Inductive Context :=
 Notation ε := Emp.
 Notation "A , C" := (Cons A C) (at level 100).
 
+Reserved Notation "Δ +++ Γ" (at level 60, right associativity).
+
+Fixpoint ContextCompose (Δ Γ : Context) : Context :=
+  match Δ with
+  | ε => Γ
+  | (A, Δ') => A, Δ' +++ Γ
+  end
+where "Δ +++ Γ" := (ContextCompose Δ Γ)
+.
+
+Fixpoint CLen (Γ : Context) : nat :=
+  match Γ with
+  | ε => O
+  | (A, Γ') => S (CLen Γ')
+  end
+.
+
 Reserved Notation "[ Γ ] ⊢ t ::: T" (at level 100, no associativity).
 
 (** Typing relation *)
 Inductive Typed : Context -> Trm -> Typ -> Prop :=
 | Tpd_Un : forall (Γ: Context), [Γ] ⊢ un ::: Un
-| Tpd_Var_O : forall (T : Typ) (Γ: Context), [T, Γ] ⊢ 𝔳(O) ::: T
-| Tpd_Var_S : forall (n : nat) (T : Typ) (Γ: Context),
-    ([Γ] ⊢ 𝔳(n) ::: T) -> [T, Γ] ⊢ 𝔳(S n) ::: T
+| Tpd_Var : forall (T : Typ) (Γ Γ' : Context),
+    [Γ +++ (T, Γ')] ⊢ 𝔳(CLen Γ) ::: T
 | Tpd_Func : forall (Γ : Context) (t : Trm) (T T' : Typ),
     ([T, Γ] ⊢ t ::: T') -> ([Γ] ⊢ (λ t) ::: (T —≻ T'))
 | Tpd_App : forall (Γ : Context) (t t' : Trm) (T T' : Typ),
     ([Γ] ⊢ t ::: (T —≻ T')) -> ([Γ] ⊢ t' ::: T) -> ([Γ] ⊢ (t · t') ::: T')
 where "[ Γ ] ⊢ t ::: T " := (Typed Γ t T).
 
+
+Theorem not_lt_O : forall n, ~ (n < 0).
+Proof.
+  intros n H.
+  inversion H.
+Qed.
+
+(** shift free m-variables (greater than or equal to  m) by n. *)
+Fixpoint shift (n : nat) (m : nat) (t : Trm) : Trm :=
+  match t with
+  | un => un
+  | 𝔳(y) => if le_dec m y then 𝔳(n + y) else 𝔳(y)
+  | λ f => λ (shift n (S m) f)
+  | t1 · t2 => (shift n m t1) · (shift n (m) t2)
+  end
+.
+
+Reserved Notation "t [ x // m ]" (at level 100, no associativity).
+
 (** substitution *)
 Fixpoint subst (t : Trm) (x : nat) (m : Trm) : Trm :=
   match t with
   | un => un
   | 𝔳(y) =>
-    match eq_nat_dec x y with
-    | left _ => m
-    | _ => 𝔳(y)
+    match lt_eq_lt_dec x y with
+    | inleft H =>
+      match H with
+      | left H' =>
+        (match y as u return x < u -> Trm with
+         | O => fun G => False_rect _ (not_lt_O _ G)
+         | S y' => fun G => 𝔳(y')
+         end)
+          H'
+      | right _ => m
+      end
+    | inright _ =>
+      𝔳(y)
     end
-  | λ f => subst f (S x) m
-  | t1 · t2 => (subst t1 x m) · (subst t2 x m)
+  | λ f => λ (f [(S x) // (shift 1 O m)])
+  | t1 · t2 => (t1 [x // m]) · (t2 [x // m])
   end
+where "t [ x // m ]" := (subst t x m)
 .
 
-Notation "t [ x // m ]" := (subst t x m) (at level 100, no associativity).
+Theorem shift_O (t : Trm) (m : nat) : shift O m t = t.
+Proof.
+  revert m.
+  induction t; cbn; intros m; trivial; try destruct le_dec; trivial.
+  + rewrite <- IHt at 2; trivial.
+  + rewrite <- IHt1 at 2; rewrite <- IHt2 at 2; trivial.
+Qed.
 
-Reserved Notation "s ↝ t" (at level 100, no associativity).
+(** Shifts can be swapped changing their arguments. *)
+Theorem shift_shift (t : Trm) (k l m n : nat) :
+  shift k l (shift m (l + n) t) = shift m (k + l + n) (shift k l t).
+Proof.
+  revert k l m n.
+  induction t; intros k l m n'; cbn;
+  trivial; repeat (try destruct le_dec; cbn);
+  trivial; try omega.
+  - match goal with
+      |- 𝔳(?A) = 𝔳(?B) => replace A with B by omega; trivial
+    end.
+  - change (S (l + n')) with ((S l) + n').
+    rewrite IHt.
+    match goal with
+      |- _ (shift _ ?A _) = _ (shift _ ?B _) => replace A with B by omega; trivial
+    end.
+  - rewrite IHt1.
+    rewrite IHt2.
+    trivial.    
+Qed.
+
+Lemma Typed_shift (Δ Δ' Γ : Context) (t : Trm) (T : Typ) :
+  ([Δ' +++ Γ]⊢ t ::: T) ->
+  [Δ' +++ Δ +++ Γ]⊢ shift (CLen Δ) (CLen Δ') t ::: T
+.
+Proof.
+  intros H.
+  remember (Δ' +++ Γ) as ξ.
+  revert Δ' Δ Heqξ.
+  set (H' := H); clearbody H'.
+  induction H; cbn in *.
+  + constructor.
+  + intros Δ' Δ Heqξ.
+    destruct le_dec.
+    admit.
+    admit.
+  + intros Δ' Δ Heqξ.
+    constructor.
+    change (T, Δ' +++ Δ +++ Γ) with ((T, Δ') +++ Δ +++ Γ).
+    apply IHTyped; trivial.
+    rewrite Heqξ.
+    trivial.
+  + intros Δ.
+    econstructor; eauto.
+Admitted.
+      
+
+  (*
+Theorem Relevant_Context (Δ Γ : Context) (n : nat) (T : Typ) :
+  ([Δ +++ Γ] ⊢ 𝔳(n) ::: T) ->
+  (n < CLen Δ) ->
+  forall Γ',   ([Δ +++ Γ'] ⊢ 𝔳(n) ::: T)
+.
+Proof.
+  revert n.
+  induction Δ; cbn; intros n H1 H2 Γ'.
+  + inversion H2.
+  + destruct n.
+    * inversion H1; subst.
+      constructor.
+    * constructor.
+      apply IHΔ.
+      inversion H1; trivial.
+      omega.
+Qed.      
+   *)
+
+Theorem Typed_Subst (Δ Δ' Γ : Context) (t t' : Trm) (T T' : Typ) :
+  ([Δ' +++ Γ] ⊢ t' ::: T') ->
+  ([Δ' +++ Δ +++ (T', Γ)] ⊢ t ::: T) ->
+  ([Δ' +++ Δ +++ Γ] ⊢ t [(CLen (Δ' +++ Δ)) // (shift (CLen Δ) (CLen Δ') t')] ::: T)
+.
+Proof.
+  intros H1 H2.
+  remember (Δ' +++ Δ +++ (T', Γ)) as Υ.
+  revert Δ Δ' t' HeqΥ H1.
+  set (H2' := H2); clearbody H2'.
+  induction H2; intros Δ Δ' s HeqΥ H1; cbn in *.
+  - constructor.
+  - destruct lt_eq_lt_dec as [[G|G]|G].
+    + destruct Γ0; [inversion G|]; cbn in *.
+      rewrite HeqΥ in H2'.
+        admit.
+    + apply Typed_shift.
+      rewrite HeqΥ in H2'.
+      admit.
+    + rewrite HeqΥ in H2'.
+      admit.
+  - constructor.
+    set (W := fun t => shift_shift t 1 0); cbn in W; rewrite W; clear W.
+    change (T, Δ' +++ Δ +++ Γ) with ((T, Δ') +++ Δ +++ Γ).
+    change (S (CLen (Δ' +++ Δ))) with (CLen (T, Δ' +++ Δ)).
+    apply IHTyped; trivial.
+    + rewrite HeqΥ.
+      trivial.
+    + admit.
+  - econstructor.
+    + apply IHTyped1; trivial.
+    + apply IHTyped2; trivial.
+Admitted.
+  
+Reserved Notation "s ↝* t" (at level 100, no associativity).
 
 Inductive Red : Trm -> Trm -> Prop :=
-| Beta : forall (t t': Trm), ((λ t) · t') ↝ (t [O // t'])
-where "s ↝ t" := (Red s t).
+| Red_Refl : forall t, t ↝* t
+| Red_Trans : forall t1 t2 t3, (t1 ↝* t2) -> (t2 ↝* t3) -> t1 ↝* t3
+| Beta : forall (t1 t2: Trm), ((λ t1) · t2) ↝* (t1 [O // t2])
+| Red_App1 : forall t1 t2 t13 t23, (t1 ↝* t13) -> (t2 ↝* t23) -> (t1 · t2) ↝* (t13 · t23)
+| Red_Func : forall t1 t2, (t1 ↝* t2) -> (λ t1) ↝* (λ t2)
+where "s ↝* t" := (Red s t).
+
+Theorem SubjectRed (Γ : Context) (t t' : Trm) (T : Typ) :
+        ([Γ] ⊢ t ::: T) -> (t ↝* t') -> ([Γ] ⊢ t' ::: T)
+.
+Proof.
+  intros H1 H2.
+  revert Γ T H1.
+  induction H2; auto;
+  intros Γ T H1; inversion H1; subst;
+  try (econstructor; eauto).
+  + inversion H3; subst.
+    set (W := Typed_Subst ε ε _ _ _ _ _ H5 H4); cbn in W.
+    rewrite shift_O in W.
+    trivial.
+Qed.
